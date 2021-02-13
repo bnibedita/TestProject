479d1d76a6ab2700d83b76bb99414149442678d7
Dell <satya.igu@gmail.com>
2021-02-13 08:13:26 +0530

    My first Code

---
+++ first/src/main/java/first_maveen_program/first/App.java
@@ -0,0 +1,13 @@
+package first_maveen_program.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
